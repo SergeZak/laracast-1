058c0b173ce6eb80b7d2c4d99c7e1b6ebed13204
sergezak <sergezak9@gmail.com>
2017-05-22 17:38:54 +0300

    finished 26

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\Welcome;
 use App\User;
 use Illuminate\Http\Request;
+
 
 class RegistrationController extends Controller
 {
@@ -26,6 +28,8 @@ class RegistrationController extends Controller
         $user = User::create($request->only(['name', 'email','password']));
 
         auth()->login($user);
+
+        \Mail::to($user)->send(new Welcome($user));
 
         return redirect()->home();
     }
